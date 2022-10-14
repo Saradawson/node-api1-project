ceea780d1af4facc5bbd9d83b6fa600fa4f3bd7c
unknown <saradawsone@gmail.com>
2022-10-14 14:12:36 -0600

    server is updating user by id

--- api/server.js
+++ api/server.js
@@ -22,7 +22,8 @@ server.post('/api/users', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({
-                message: "There was an error while saving the user to the database"
+                message: "There was an error while saving the user to the database",
+                err: err.message
             })
         })
 
@@ -61,12 +62,55 @@ server.get('/api/users/:id', (req, res) => {
                 stack: err.stack,
             })
         })
-}) 
+})
 
-server.use('*', (req, res) => {
-    res.status(404).json({
-        message: 'not found'
-    })
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    User.remove(id)
+        .then(deletedUser => {
+            if(!deletedUser){
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist" 
+                })
+            }else{
+                res.status(200).json({
+                    id: deletedUser.id,
+                    name: deletedUser.name,
+                    bio: deletedUser.bio
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The user could not be removed",
+                err: err.message 
+            })
+        })
+})
+
+server.put('/api/users/:id', async (req, res) => {
+    try{
+       const possibleUser = await User.findById(req.params.id)
+       if(!possibleUser){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        })
+       } else {
+         if(!req.body.name || !req.body.bio){
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
+            })
+         }else{
+            const updatedUser = await User.update(req.params.id, req.body)
+            res.status(200).json(updatedUser)
+         }
+       }
+    } catch (err) {
+        res.status(500).json({
+            message: "The user information could not be modified",
+            err: err.message
+        })
+    }
 })
 
 module.exports = server; 
